76643dca54137df9071dbc7fc4e38facb51f744a
Dev1 <dev1@iMac-de-JCA.local>
2019-11-28 13:54:30 +0100

    miércoles,27nov/appListaComModifAltaModifBorrar-TEST

--- json/APPComentarios/funciones.js
+++ json/APPComentarios/funciones.js
@@ -1,13 +1,19 @@
-function guardarLocal(clave, valor) { localStorage.setItem(clave, valor); console.log("Clave-saved " + clave + "[" + valor + "] = " + verLocal(clave)); }
+function guardarLocal(clave, valor) {
+    localStorage.setItem(clave, valor); console.log("Clave-saved " + clave + "[" + valor + "] = " + verLocal(clave));
+}
 function verLocal(clave) { valor = localStorage.getItem(clave); console.log("Clave-recuperado:" + clave + "-" + valor); return valor; }
 
 // llamada GET
 function llamarSrv(uri, req, callback) {
     console.log("llamarSrv() ... entra ...");
+
+    console.log("req");
+    console.log(req);
+
     xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4) {
-            if (xmlHttp.status == 200) {
+            if (xmlHttp.status == 200 || 201 ) {
                 //var cuerpo = JSON.parse(xmlHttp.responseText);
                 console.log(" ** OK ** ... " + xmlHttp.status);
                 console.log(" Retorno correcto. ... ");
@@ -17,11 +23,29 @@ function llamarSrv(uri, req, callback) {
                 console.log(" ** Error ** en el servicio ... " + xmlHttp.status);
                 sacarErrorG();
             }
-        } else { console.log(" En proceso (xmlHttp.readyState) ... " + xmlHttp.readyState); }
+        } // else { console.log(" En proceso (xmlHttp.readyState) ... " + xmlHttp.readyState); }
     }
-    //TO-DO implemetar POST
-    xmlHttp.open('GET', uri, true);
-    xmlHttp.send(null); //get: NO datos en body
+    // **TESTING**  implemetar POST
+
+    if (req==null){
+        xmlHttp.open('GET', uri, true);
+        xmlHttp.send(null); //get: NO datos en body
+    }else{
+/*
+    xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = comprobarEstado;
+            xmlHttp.open('POST', URI_SERVICE, true);
+            //completar petición pOsT
+            midni = new Dni(DNI.value, LETRA.value);
+            console.log(midni);
+            xmlHttp.setRequestHeader("content-type", "application/json");
+            xmlHttp.send(midni.toString()); //post: datos en body
+*/
+        xmlHttp.open(req.metodo, uri, true);
+        xmlHttp.setRequestHeader("content-type", "application/json");
+        xmlHttp.send(JSON.stringify( req.bd)); //?: datos en req.mensaje
+        //xmlHttp.send(req.mensaje); //?: datos en req.mensaje
+    }
     console.log("llamarSrv() ... sale ...");
 }
 function sacarErrorG() {
